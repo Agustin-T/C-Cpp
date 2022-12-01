9cc676a4c3e61bd93b66dfc600f421754646eaba
Agustin-T <65094491+Agustin-T@users.noreply.github.com>
2022-12-01 20:31:54 -0300

    FunctionsMath
    > functions Base Change modificated
    >  Error: Not show me the error typing

--- Exercise.c
+++ Exercise.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
 void main(){
-    printf("Welcome to my firs GIthub repository");
+    printf("Welcome to my firs GIthub reposit")
+
+    int parcelen = 0
+    int parcela = 1
+    int p == 2;
+    
+
+
 }
--- FunctionsMath.c
+++ FunctionsMath.c
@@ -1,26 +1,62 @@
 #include <stdio.h>
+#include <String.h>
 
 #include "StyleCMD.c"
 
-float changeBase(){
+// 2.0 calcular numeros punto flotante
 
-    float number, base, baseFind;
+
+
+int changeBase(){
+
+    int number, base, baseFind, numberFind, rest;
+
+    int elements;
+    char file [50]= {" "};
+    char fileOrder[50] = {" "}
+
+    
 
     ast(3);
     printf("\n- Input the current number\n > ");
-    scanf("%f", &number);
+    scanf("%i", &number);
     printf("\n- Input the current base\n > ");
-    scanf("%f", &base);
+    scanf("%i", &base);
     printf("\n- Input the base number that you want to change\n > ");
-    scanf("%f", &baseFind);
+    scanf("%i", &baseFind);
     ast(3);
 
-    // binario
+    // number to decimal
 
-    // octal
+    numberFind = number;
+    
+    for(int i=0; numberFind > 10; i++){
+        numberFind /= 10;
+        rest = numberFind % 10;
+        file[i] = (char)rest;
+        
+    }
 
-    // decimal
-    printf("\n Number: %.2f\n Initial Base: %.2f\n Final Base: %.2f\n", number, base, baseFind);
+    // ask if the baseFind is 10
+    if(baseFind != 10){
+        // order
+        elements = len(file);
+        for(int i=0; i >= elements, i++){
+            for(int j = elements; j >= i; j--){
+                fileOrder[i] = file[j];
+            }
+        }
+    
+   // decimal to 
+        for(int i=0; )
+    
+    
+    }
+    
+    // binary - hexadecimal
+
+    // answers
+    printf("\n Number: %i\n Initial Base: %i\n\n NumberFind: %c\n Final Base: %i\n", number, base, numberFind, baseFind);
     ast(1);
 }
 
@@ -32,21 +68,38 @@ void main (){
     while (1){
         ast(2);
         printf("\n> 1 - Chenge of base of the numbers");
-        printf("\n> 2 - ");
-        printf("\n> 3 - ");
+        printf("\n> 2 - Resolution of ecuations");
+        printf("\n> 3 - Calc of the matrix");
+        printf("\n> 4 - Max-Min Parameters");
+        printf("\n> 5 - Exit");
         printf("\n> Input your option\n> ");
         scanf("%i", &op);
         ast(2);
 
-        if(op == 1){
+        if(op == 1){ 
             changeBase();
         }
-        else if(op == 3){
+        // else if(op == 2){
+        //     // input the ecuations
+        //     // change the ecuations
+        // }
+        // else if(op == 3){
+        //     // input de matrix
+        //     // select matrix clasification
+        //     //  select method(chio, sarrus, frobeniuns, rouche frobenius)   
+        //     // calc and return matrix identify 
+        // }
+        // else if(op == 4){
+        //     // Input the functions - limits
+        //     // max o min
+        //     // calc and return
+        // }
+        else if(op == 5){
             ast(1);
             printf("\n\t\t\tOut the Promgram...\n\n");
             ast(1);
-            
             break;
+
         }else{
             ast(1);
             printf("\n\t\t\twrong, input again\n");
